12d184fc7f3939fd75630ddfc8332dcb5aba49e2
Oscar Callisaya <oreynaldo.cl@gmail.com>
2021-07-13 18:05:24 -0400

    Added Ninject for bootstrap of application.

---
+++ PeopleViewer.Ninject/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Ninject;
+using PeopleViewer.Common;
+using PersonDataReader.CSV;
+using PersonDataReader.Service;
+using PersonReader.Caching;
+using System.Windows;
+
+namespace PeopleViewer.Ninject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        IKernel Container = new StandardKernel();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            Application.Current.MainWindow.Title = "With Dependency Injection - Ninject";
+            Application.Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+
+            Container.Bind<IPersonReader>().To<CSVReader>()
+                .InSingletonScope();
+        }
+
+        private void ComposeObjects()
+        {
+            Application.Current.MainWindow = Container.Get<PeopleViewerWindow>();
+        }
+    }
+}
---
+++ PeopleViewer.Ninject/Converters.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PeopleViewer.Ninject
+{
+    public class DecadeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int year = ((DateTime)value).Year;
+            return string.Format("{0}0s", year.ToString().Substring(0, 3));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RatingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int rating = (int)value;
+            return string.Format("{0}/10 Stars", rating.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RatingStarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int rating = (int)value;
+            string output = string.Empty;
+            return output.PadLeft(rating, '*');
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string input = (string)value;
+            //int rating = 0;
+
+            //foreach (var ch in input)
+            //    if (ch == '*')
+            //        rating++;
+
+            //return rating;
+
+            return input.Count(c => c == '*');
+        }
+    }
+
+    public class DecadeBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int decade = (((DateTime)value).Year / 10) * 10;
+
+            return decade switch
+            {
+                1970 => new SolidColorBrush(Colors.Maroon),
+                1980 => new SolidColorBrush(Colors.DarkGreen),
+                1990 => new SolidColorBrush(Colors.DarkSlateBlue),
+                2000 => new SolidColorBrush(Colors.CadetBlue),
+                _ => new SolidColorBrush(Colors.DarkSlateGray),
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
